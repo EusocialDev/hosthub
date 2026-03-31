d1619ba4432ed6e83f69074835b5057da74b9d8e
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2026-03-31 14:36:45 -0400

    register new models in admin

--- test_1/testendpoint/admin.py
+++ test_1/testendpoint/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Call
+from .models import Call, Account, Location, PhoneNumber
 
-
+admin.site.register(Account)
+admin.site.register(Location)
+admin.site.register(PhoneNumber)
 admin.site.register(Call)
 
 # Register your models here.
