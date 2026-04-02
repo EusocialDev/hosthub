c389515010fee5d5f2272dae1f5f3e2de0b9842c
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2026-04-02 17:12:08 -0400

    register UserAccess model to admin page

--- test_1/testendpoint/admin.py
+++ test_1/testendpoint/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Call, Account, Location, PhoneNumber
+from .models import Call, Account, Location, PhoneNumber, UserAccess
 
 admin.site.register(Account)
 admin.site.register(Location)
 admin.site.register(PhoneNumber)
 admin.site.register(Call)
+
+@admin.register(UserAccess)
+class UserAccessAdmin(admin.ModelAdmin):
+    list_display = ("user", "account", "role", "is_active")
+    filter_horizontal = ("locations",)
 
 # Register your models here.
