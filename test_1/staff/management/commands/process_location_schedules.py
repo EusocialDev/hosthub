7433646b62975ffd22aebbf2d542055d33a01910
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2026-04-16 18:42:48 -0400

    add to cron job

--- test_1/staff/management/commands/process_location_schedules.py
+++ test_1/staff/management/commands/process_location_schedules.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
-from staff.services.location_hours import process_due_location_schedules
+from staff.services.location_hours import process_due_location_schedules, reconcile_location_bland_state
 
 class Command(BaseCommand):
     help = "Processes location schedule transition and syncs Bland Pathways"
 
     def handle(self, *args, **options):
         process_due_location_schedules()
+        reconcile_location_bland_state()
         self.stdout.write(self.style.SUCCESS("Successfully processed location schedules."))
